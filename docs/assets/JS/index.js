abbfa26b6bdf8c03c20ae01e57e6a4a50e32cfb8
Juanin92 <j.claveria19@com>
2024-11-06 21:03:47 -0300

    Change Directory

--- docs/assets/JS/index.js
+++ docs/assets/JS/index.js
@@ -26,10 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const icon = document.getElementById("iconVisibility");
       if (!darkTheme.disabled) {
-        icon.src = "/assets/Image/Icons/sun.png";
+        icon.src = "/docs/assets/Image/Icons/sun.png";
         icon.alt = "sun.png";
       } else {
-        icon.src = "/assets/Image/Icons/moon.png";
+        icon.src = "/docs/assets/Image/Icons/moon.png";
         icon.alt = "moon.png";
       }
     });
