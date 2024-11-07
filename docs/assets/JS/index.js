6afaa5cc91fe9423abcf64638ee767ce8f6c2a80
Juanin92 <j.claveria19@com>
2024-11-07 15:02:57 -0300

    Fix toggleButton

--- docs/assets/JS/index.js
+++ docs/assets/JS/index.js
@@ -1,17 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const toggler = document.querySelector(".navbar-toggler");
-//   const mainContent = document.querySelector("main");
-
-//   navbarNav.classList.remove("show");
-//   toggler.setAttribute("aria-expanded", "false");
-
-//   // Evento de clic para manejar el despliegue manual
-//   toggler.addEventListener("click", function () {
-//     const isExpanded = toggler.getAttribute("aria-expanded") === "true";
-//     toggler.setAttribute("aria-expanded", !isExpanded); // Cambia el valor de aria-expanded
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const visibilityToggle = document.getElementById("themeToggle");
   const lightTheme = document.getElementById("lightTheme");
